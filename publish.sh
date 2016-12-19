655050a5e4acb23c9553b899ae69d3478986e6e7
Ali Davut <adavut@gmail.com>
2016-12-19 20:56:27 +0300

    publish script

---
+++ publish.sh
@@ -0,0 +1,20 @@
+DIRS="
+todos
+"
+
+rm -rf ./build
+
+for dir in $DIRS; do
+  (cd $dir && npm run build)
+  mkdir -p ./build/$dir
+  cp -R ./$dir/build/. ./build/$dir
+done
+
+cd ./build
+git init
+git commit --allow-empty -m 'update apps'
+git checkout -b gh-pages
+touch .nojekyll
+git add .
+git commit -am 'update apps'
+git push git@github.com:buildize/hacksaw-examples gh-pages --force
