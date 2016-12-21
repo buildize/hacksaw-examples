0c3795694dc6d59f46b72b0514fe3cc126b01b93
Ali Davut <adavut@gmail.com>
2016-12-21 05:40:07 +0300

    use hash history

--- github/src/routes.js
+++ github/src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useRouterHistory, Router, Route } from 'react-router';
-import { createHistory } from 'history';
+import { createHashHistory } from 'history';
 
 import AppContainer from './containers/app-container'
 import SearchContainer from './containers/search-container';
 import UserContainer from './containers/user-container';
 
-export const history = useRouterHistory(createHistory)({ basename: '/github' });
+export const history = useRouterHistory(createHashHistory)({ basename: '/' });
 
 export default (
   <Router history={history}>
